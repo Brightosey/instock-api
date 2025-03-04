0c9a159b495ae3d99f9d864d8055c0abae25b598
Vivian <59620790+Vivianwcao@users.noreply.github.com>
2025-03-03 17:53:44 -0800

    back-end setup, Use the CORS and express.json() Express middleware.

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 import express from "express";
 import "dotenv/config";
+import cors from "cors";
+
+import initKnex from "knex";
+import configuration from "./knexfile.js";
+const knex = initKnex(configuration);
+
+const { PORT, DB_HOST, DB_NAME, DB_USER, DB_PASSWORD } = process.env;
 
 const app = express();
 const router = express.Router();
-const { PORT, DB_HOST, DB_NAME, DB_USER, DB_PASSWORD } = process.env;
+app.use(cors());
+
+app.use(express.json());
+app.use(express.static("public"));
 
 app.get("/", (_req, res) =>
 	res.send(`Welcome to inStock api by team Witty Willows!`)
