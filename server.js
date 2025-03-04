0530479dbb4c07b0e530f26223a68a11aea1c154
Vivian <59620790+Vivianwcao@users.noreply.github.com>
2025-03-04 11:12:24 -0800

    configure routeson server.js

--- server.js
+++ server.js
@@ -1,22 +1,49 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
-
 import initKnex from "knex";
 import configuration from "./knexfile.js";
+
 const knex = initKnex(configuration);
 
 const { PORT, DB_HOST, DB_NAME, DB_USER, DB_PASSWORD } = process.env;
 
 const app = express();
 const router = express.Router();
-app.use(cors());
 
+app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
+app.get("/api/warehouses", async (_req, res) => {
+	try {
+		const warehouses = await knex("warehouses");
+		res.status(200).json(warehouses);
+	} catch (err) {
+		console.log(err);
+		res.status(500).json({ message: "Error getting warehouses" });
+	}
+});
+
+app.get("/api/warehouses/:id", async (req, res) => {
+	try {
+		const warehouse = await knex("warehouses")
+			.where({ id: req.params.id })
+			.first();
+		if (!warehouse) {
+			return res
+				.status(404)
+				.json({ message: `Warehouse with id ${req.params.id} not found` });
+		}
+		res.status(200).json(warehouse);
+	} catch (err) {
+		console.log(err);
+		res.status(500).json({ message: "Error getting warehouse" });
+	}
+});
+
 app.get("/", (_req, res) =>
-	res.send(`Welcome to inStock api by team Witty Willows!`)
+	res.send(`Welcome to the InStock API by Team Witty Willows!`)
 );
 
-app.listen(PORT, () => console.log(`app running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
