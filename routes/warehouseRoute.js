9cd05474ee7e40a6feffc46780618cdce4e68043
Brightosey <Brightosey@users.noreply.github.com>
2025-06-22 15:11:21 -0400

    modified warehouse route

--- routes/warehouseRoute.js
+++ routes/warehouseRoute.js
@@ -28,7 +28,7 @@ router.get("/:id", async (req, res) => {
     res.status(200).json(warehouse);
   } catch (err) {
     console.log(err);
-    res.status(500).json({ message: "Error getting warehouse" });
+    res.status(500).json({ message: "Error in getting warehouse" });
   }
 });
 
